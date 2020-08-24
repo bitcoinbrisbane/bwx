abae1cdac7cc118689b34b15fbddea50e496948f
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-08-24 13:59:24 +1000

    Update layout

--- src/pages/PayoutAddressPage.js
+++ src/pages/PayoutAddressPage.js
@@ -15,8 +15,10 @@ const PayoutAddressPage = () => {
   });
 
   const payoutAddress = () => {
-    if (localStorage.getItem("bwx_address") === "" || localStorage.getItem("bwx_address") === undefined)
-    {
+    if (
+      localStorage.getItem("bwx_address") === "" ||
+      localStorage.getItem("bwx_address") === undefined
+    ) {
       return (
         <div className="alert alert-danger">
           You have not entered a payout address!
@@ -27,32 +29,39 @@ const PayoutAddressPage = () => {
 
   return (
     <Layout>
-      <div className="container-fluid py-5">
-        <h2>Payout Address</h2>
+      <div className="container">
+        <div className="row">
+          <div class="col-md-6">
+            <h2>Payout Address</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div class="col-md-6">
+            {payoutAddress()}
 
-        {payoutAddress()}
+            <p>
+              In order to use Bet With Coins, we require your bitcoin address
+              where you will receive payouts. These are not stored on our
+              server, but in your local machine's browser cookie. This can be
+              deleted anytime by clearing your cookies.
+            </p>
 
-        <p>
-          In order to use Bet With Coins, we require your bitcoin address where
-          you will receive payouts. These are not stored on our server, but in
-          your local machine's browser cookie. This can be deleted anytime by
-          clearing your cookies.
-        </p>
-
-        <p>
-          <b>
-            Please make sure you are in control of this address and DO NOT use
-            an exchange address.
-          </b>
-        </p>
-        <div className="panel panel-default">
-          <div className="panel-body">
+            <p>
+              <b>
+                Please make sure you are in control of this address and DO NOT
+                use an exchange address.
+              </b>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div class="col-md-6">
             <h3>Your Bitcoin Address</h3>
 
-            <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={formik.handleSubmit} className="col-12">
               <div className="form-group">
                 <label>
-                  Your bitcoin address where payouts will be received.
+                  Enter the bitcoin address where payouts will be received.
                 </label>
                 <input
                   id="payout"
