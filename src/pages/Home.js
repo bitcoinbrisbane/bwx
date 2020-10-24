4de832ae43431649895499dafa49269aa5fd398e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-10-25 09:41:03 +1000

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,12 @@ const Home = () => {
             <div className="col">
 
             <h1>Bet With Coins</h1>
-            <p>BWCs is a privacy focused crypto sports betting site.  We do not capture personal infomation and only ask our punters to supply a payment address.  We do not hold any of our customers funds, and payout immediately after the even has finished and payments confirmed on the blockchain.</p>
-            <p>We encourage all our punters to use PGP encrypted mail, should you need to contact us.  Our PGP public key can be found at </p>
+            <p>BWCs is a privacy focused crypto sports betting site.  We do not capture any personal infomation and only ask our punters to supply a payout address.  We do not hold any of our customers funds and payout immediately once bets have been confirmed on the blockchain.</p>
+            <p>We encourage all our punters to use PGP encrypted mail should you need to contact us.  Our PGP public key can be found at </p>
           </div>
         </div>
       </div>
-
     </Layout>
-
   );
 };
 
