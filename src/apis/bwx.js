3e45a9d39e8d99c316f69f5a3c99e5fe00f18dd4
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-01-17 11:06:06 +1000

    Hard code url

--- src/apis/bwx.js
+++ src/apis/bwx.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const open = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://139.180.172.190",
+  //baseURL: process.env.REACT_APP_API_URL || "http://139.180.172.190",
+  baseURL: "http://139.180.172.190",
   headers: {
     "Cache-Control": "no-store"
   }
 });
 
 const secure = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://139.180.172.190",
+  //baseURL: process.env.REACT_APP_API_URL || "http://139.180.172.190",
+  baseURL: "http://139.180.172.190",
   headers: {
     "Cache-Control": "no-store"
   }
