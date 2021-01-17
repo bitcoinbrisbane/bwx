bf3795a3042ca032b5d3335b8b817624189af137
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-01-17 11:08:49 +1000

    Logs

--- src/apis/bwx.js
+++ src/apis/bwx.js
@@ -16,16 +16,16 @@ const secure = axios.create({
   }
 });
 
-secure.interceptors.request.use(
-  async config => {
-    const user = JSON.parse(window.localStorage.getItem("user"));
-    const { token } = user;
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
+// secure.interceptors.request.use(
+//   async config => {
+//     const user = JSON.parse(window.localStorage.getItem("user"));
+//     const { token } = user;
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default { open, secure };
--- src/pages/HorseRacingPage.js
+++ src/pages/HorseRacingPage.js
@@ -14,6 +14,7 @@ const HorseRacingPage = () => {
   const defaultValues = {"meets": []};
   const viewModel = data || defaultValues;
 
+  console.log("HR data");
   console.log(data);
 
   return (
