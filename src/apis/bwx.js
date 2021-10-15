13d08a7925eaa23ae2afdf5734acd5f244b2a791
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-10-16 09:17:39 +1000

    Chore: Add api and axios

---
+++ src/apis/bwx.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const open = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1",
+  headers: {
+    "Cache-Control": "no-store"
+  }
+});
+
+const secure = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1",
+  headers: {
+    "Cache-Control": "no-store"
+  }
+});
+
+export default { open, secure };
