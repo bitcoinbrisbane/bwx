1e438e15015e6d0adbe9a5f91f43e8a148074de8
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-10-23 08:01:42 +1000

    WIP

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Nav from "./Nav";
 
+import "../cover.css";
+
 const Layout = ({header, children, noHeader, activeTab}) => (
   <>
-    <header>{noHeader || header || <Nav activeTab={activeTab} />}</header>
-    <main>
+    <header className="masthead mb-auto">
+      <div className="inner">
+      <h3 className="masthead-brand">BWX</h3>
+        {noHeader || header || <Nav activeTab={activeTab} />}
+      </div>
+    </header>
+    <main role="main" className="inner cover">
       <div>{children}</div>
     </main>
   </>
